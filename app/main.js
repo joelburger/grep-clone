18fbffb0ca474dec442838fc28b0ae88321e677a
joel <joel@burgerjoe.dev>
2025-02-04 20:44:00 +1300

    submit

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 function matchPattern(inputLine, pattern) {
-  if (pattern.length === 1) {
-    return inputLine.includes(pattern);
+  if (pattern.length > 0) {
+    const regex = new RegExp(pattern);
+    return regex.test(inputLine);
   } else {
     throw new Error(`Unhandled pattern ${pattern}`);
   }
@@ -10,14 +11,18 @@ function main() {
   const pattern = process.argv[3];
   const inputLine = require('fs').readFileSync(0, 'utf-8').trim();
 
+  console.log({ pattern, inputLine });
+
   if (process.argv[2] !== '-E') {
     console.log("Expected first argument to be '-E'");
     process.exit(1);
   }
 
   if (matchPattern(inputLine, pattern)) {
+    console.log('pattern matched');
     process.exit(0);
   } else {
+    console.log('no match');
     process.exit(1);
   }
 }
