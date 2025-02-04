10c39589395f8bc13e9538d8a7c9d2c775e6d416
joel <joel@burgerjoe.dev>
2025-02-04 20:24:34 +1300

    test

--- app/main.js
+++ app/main.js
@@ -8,22 +8,18 @@ function matchPattern(inputLine, pattern) {
 
 function main() {
   const pattern = process.argv[3];
-  const inputLine = require("fs").readFileSync(0, "utf-8").trim();
+  const inputLine = require('fs').readFileSync(0, 'utf-8').trim();
 
-  if (process.argv[2] !== "-E") {
+  if (process.argv[2] !== '-E') {
     console.log("Expected first argument to be '-E'");
     process.exit(1);
   }
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.error("Logs from your program will appear here");
-
-  // Uncomment this block to pass the first stage
-  // if (matchPattern(inputLine, pattern)) {
-  //   process.exit(0);
-  // } else {
-  //   process.exit(1);
-  // }
+  if (matchPattern(inputLine, pattern)) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
 }
 
 main();
